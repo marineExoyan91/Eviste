3b2499c6e4df09c31c4e37b4653b8c96d76052fa
marineExoyan91 <marine.exoyan91@gmail.com>
2022-09-20 13:06:57 +0400

    ev

---
+++ script.js
@@ -0,0 +1,84 @@
+function f(){
+	text_block.style.display = "block";
+	manager.style.display ="none";
+	project_manager.style.display = "none";
+	employee_text.style.display = "none";
+	client_text.style.display = "none";
+
+}
+
+function a(){
+	manager.style.display = "block";
+	text_block.style.display = "none";
+	project_manager.style.display = "none";
+	employee_text.style.display = "none";
+	client_text.style.display = "none";
+}
+function s(){
+	project_manager.style.display = "block";
+	text_block.style.display = "none";
+	manager.style.display ="none";
+	employee_text.style.display = "none";
+	client_text.style.display = "none";
+}
+
+function block(){
+	employee_text.style.display = "block";
+	text_block.style.display = "none";
+	manager.style.display ="none";
+	project_manager.style.display = "none";
+	client_text.style.display = "none";
+}
+
+function z(){
+	client_text.style.display = "block";
+	text_block.style.display = "none";
+	manager.style.display ="none";
+	project_manager.style.display = "none";
+	employee_text.style.display = "none";
+}
+
+function upp(){
+	block_text.style.display = "block";
+	down.style.display = "block";
+	up.style.display = "none";
+}
+function dow(){
+	block_text.style.display = "none";
+	down.style.display = "none";
+	up.style.display = "block";
+}
+function xx(){
+	none_text.style.display = "block";
+	down_icon.style.display = "block";
+	up_icon.style.display = "none";
+}
+
+function vv(){
+	none_text.style.display = "none";
+	down_icon.style.display = "none";
+	up_icon.style.display = "block";
+}
+
+function nn(){
+	how_none.style.display = "block";
+	verev.style.display = "none";
+	nerqev.style.display = "block";
+}
+
+function cc(){
+	how_none.style.display = "none";
+	verev.style.display = "block";
+	nerqev.style.display = "none";
+}
+
+function on(){
+	who_text.style.display = "block";
+	pod.style.display = "block";
+	vniz.style.display = "none";
+}
+function bl(){
+	who_text.style.display = "none";
+	pod.style.display = "none";
+	vniz.style.display = "block";
+}
